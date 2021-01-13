99c73def87fc56548f675cd6400371c895aee98e
DenisFZelaya <48973732+DenisFZelaya@users.noreply.github.com>
2021-01-13 15:42:10 -0600

    Initial commit

---
+++ ConsoleApp-T-List/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic; // No olvidar importar
+
+namespace ConsoleApp_T_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Create a list of strings using collection initializer  
+            //var names = new List<string>() { "Sonoo", "Vimal", "Ratan", "Love" };
+
+            //Create a list of strings
+            var names = new List<string>();
+
+            names.Add("Denis Zelaya");
+            names.Add("Mellisa");
+            names.Add("Maite");
+
+            foreach (var name in names) 
+            {
+                Console.WriteLine(name);
+            }
+
+            //Similar a un array en js
+           
+        }
+    }
+}
